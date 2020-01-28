ff075f9a19d8042c4ea9aa8680cafec100a36d12
Shapparanks <Jacob.Shapiro1@marist.edu>
2020-01-27 19:43:40 -0500

    Second Python Script

---
+++ Goodbye.py
@@ -0,0 +1,8 @@
+# Added a Good Bye to the Program. 
+# Introduction to Programming
+# Author: Jacob Shapiro
+# Date: January 27th, 2020
+def main():
+    print("Hello, instructor!")
+    print("Good-bye!")
+main()
