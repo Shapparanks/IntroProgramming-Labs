5152369583907622271a2b7ff215786e283a9447
Shapparanks <Jacob.Shapiro1@marist.edu>
2020-02-03 19:57:09 -0500

    Solution to Mad Libs Exercise.

---
+++ madlib.py
@@ -0,0 +1,12 @@
+print("Welcome to Madlibs!")
+
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+pluralN = input("Enter a plural noun: ")
+noun2 = input("Enter another noun: ")
+emotion = input("Enter an emotion: ")
+place = input("Enter a place: ")
+WeirdF = input("Enter a weird food: ")
+
+print("There once was a very small " + noun + " named Little Bill."" He wanted to learn how to " + verb + ", but he's having a hard time trying because he has no legs."" After trying for a few months, he wasn't improving."" One day after practicing for hours on end, his " + pluralN + " came out of a tree house and gave him a new " + noun2 + " that was red with flames on the side."" Little Billie was so " + emotion + "."" He couldn't believe the his " + pluralN + " got him this."" Now he can finally go to " + place + " and get his favorite thing to eat, " + WeirdF + " with a side of fried pickles."" The end.")
+
