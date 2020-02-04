cdc250aee3948c10d10a2129cbfa8242f11aaddf
Shapparanks <60368681+Shapparanks@users.noreply.github.com>
2020-02-03 20:09:35 -0500

    Update madlib.py
    Added informational lines between the lines of code, showing each part represents.

--- madlib.py
+++ madlib.py
@@ -1,5 +1,7 @@
+# Title string for program 
 print("Welcome to Madlibs!")
 
+#Selection of inputs for the user to fill in for Mad Lib Story 
 noun = input("Enter a noun: ")
 verb = input("Enter a verb: ")
 pluralN = input("Enter a plural noun: ")
@@ -8,5 +10,6 @@ emotion = input("Enter an emotion: ")
 place = input("Enter a place: ")
 WeirdF = input("Enter a weird food: ")
 
+#The story which includes the inputs within the string
 print("There once was a very small " + noun + " named Little Bill."" He wanted to learn how to " + verb + ", but he's having a hard time trying because he has no legs."" After trying for a few months, he wasn't improving."" One day after practicing for hours on end, his " + pluralN + " came out of a tree house and gave him a new " + noun2 + " that was red with flames on the side."" Little Billie was so " + emotion + "."" He couldn't believe the his " + pluralN + " got him this."" Now he can finally go to " + place + " and get his favorite thing to eat, " + WeirdF + " with a side of fried pickles."" The end.")
 
