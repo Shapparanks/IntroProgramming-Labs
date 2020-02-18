d8b4dde25e81abb5d4e03926808f6faaf33e9570
Shapparanks <60368681+Shapparanks@users.noreply.github.com>
2020-02-17 19:02:52 -0500

    Initial version of a simple guessing game
    Learning how to use if & else statements by making an animal guessing game.

---
+++ guessing-game.py
@@ -0,0 +1,18 @@
+def start():
+    pass
+print("Welcome to the Python Guessing Game.")
+print("I am currently thinking of an animal for you to guess...")
+animal = "Dog"
+while animal := "Dog":
+    userchoice = input("Ok, I got one! Try and guess which animal I am thinking of: ")
+    if userchoice == animal:
+        print("Congratulations, you guessed correctly. Well done!")
+        break
+    else:
+        print("I'm sorry, that is incorrect. Try again!")
+
+
+
+
+    start()
+
